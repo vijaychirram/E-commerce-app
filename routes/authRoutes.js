f99797c0b99a0fa0733afaccae5a7ea5a13a3ad2
Vijay <vijaychirram@gmail.com>
2024-09-28 12:03:20 -0500

    private routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,4 +13,8 @@ router.post("/login", loginController);
 
 router.get("/test", requireSignIn, isAdmin, testController);
 
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
 export default router;
