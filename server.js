4dfc59145689c505af2a5073b80a8aa1f0fc8c29
Vijay <vijaychirram@gmail.com>
2024-10-02 13:36:34 -0500

    products api crud

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
 dotenv.config();
@@ -18,6 +20,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1>Welocome to e commerce app mern stack</h1>");
